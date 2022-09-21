adbb8e232bd769d12fd88e2d24b4ee6562ace040
Igor <igoraraujobda@gmail.com>
2022-09-21 18:15:43 -0300

    remoção da pasta sessao

--- Fundamentos/secao-4-introducao-a-javascript/Introdução-a-javascript/dia-1-primeiros-passos/para-fixar/scripts3.js
+++
@@ -1,9 +0,0 @@
-const score = 45 ;
-
-if (score >= 80){
-  console.log ('Parabéns, você foi aprovada(o)!');
-} else if (score <80 && score >=60){
-  console.log ('Você está na nossa lista de espera');
-} else {
-  console.log ('Vocẽ foi reprovado(a)');
-}
