adc64d57dd0189f293d536c2f06ba919bcb2bbd0
Igor <igoraraujobda@gmail.com>
2022-09-22 12:19:52 -0300

    exercicio imposto de renda

--- Fundamentos/secao-4-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
@@ -45,7 +45,7 @@ switch (piece.toLowerCase()) {
 } */
 
 
-const compra = 2000;
+/* const compra = 2000;
 const venda = 5000;
 
 
@@ -56,3 +56,39 @@ if (compra < 0 || venda < 0) {
   const lucro = venda - custo;
   console.log(lucro);
 }
+ */
+
+let salarioBruto = 3000.00;
+let inss;
+let ir;
+
+
+if (salarioBruto <= 1556.94) {
+  inss = salarioBruto * 0.08;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  inss = salarioBruto * 0.09;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  inss = salarioBruto * 0.11;
+} else if (salarioBruto >= 5189.82) {
+  inss = 570.88;
+}
+
+let salarioBase = salarioBruto - inss; 
+
+if (salarioBase <= 1903.98) {
+  ir = 0;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  ir = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase >= 2826.22 && salarioBase <= 3751.05) {
+  ir = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  ir = (salarioBase * 0.225) - 636.16;
+} else if (salarioBase > 4994.68) {
+  ir = (salarioBase * 0.275) - 869.36;
+}
+
+salarioLiquido = salarioBruto - inss - ir;
+
+console.log(salarioLiquido);
+
+
