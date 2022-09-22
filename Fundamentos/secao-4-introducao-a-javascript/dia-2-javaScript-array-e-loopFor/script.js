04437403a989a88c721e49ffc078f0bd00a65df6
Igor <igoraraujobda@gmail.com>
2022-09-22 16:13:42 -0300

    percorrendo array

--- Fundamentos/secao-4-introducao-a-javascript/dia-2-javaScript-array-e-loopFor/script.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-2-javaScript-array-e-loopFor/script.js
@@ -1,9 +1,47 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+/* let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
 for (let name of names){
   console.log(name)
 }
-
+________________________________________________________-
 for(i=0; i<names.length;i++){
   console.log(names[i]);
 }
+
+
+function dice () { 
+  return Math.ceil(Math.random() * 6);
+}
+let d1 = dice();
+let d2 = dice();
+while (d1 === d2) {
+  d2 = dice();
+} */
+
+/* 🚀 Lidando com Arrays */
+
+/* Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+Some todos os valores contidos no array e imprima o resultado;
+
+Calcule e imprima a média aritmética dos valores contidos no array;
+
+A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+Utilizando for, descubra qual o menor valor contido no array e imprima-o;
+
+Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2. */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i=0; i<numbers.length; i++){
+  console.log(numbers[i]);
+}
+
