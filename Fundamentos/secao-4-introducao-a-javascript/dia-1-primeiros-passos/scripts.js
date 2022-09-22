cfb9ba1d1e9f65e75af593776fbbf2003d332a54
Igor <igoraraujobda@gmail.com>
2022-09-22 10:14:48 -0300

    5 programas de operação matemática básica

---
+++ Fundamentos/secao-4-introducao-a-javascript/dia-1-primeiros-passos/scripts.js
@@ -0,0 +1,15 @@
+let a = 100;
+let b = 200;
+
+let soma = a+b;
+let sub = a-b;
+let mult = a*b;
+let div = a/b;
+let mod = a%b;
+
+console.log(soma);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(mod);
+
