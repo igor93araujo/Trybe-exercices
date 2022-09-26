2e9a7872f20c7c1fbe67c365b3c0576dfc153c1e
Igor <igoraraujobda@gmail.com>
2022-09-26 14:17:01 -0300

    Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console.

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
@@ -6,3 +6,13 @@ let info = {
 };
 
 console.log("Bem-vinda, "+ info.personagem);
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console.
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+};
+console.log(info['recorrente']);
