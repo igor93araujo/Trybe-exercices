9ddd3db03748007037e96d70060602a6fe8095a3
Igor <igoraraujobda@gmail.com>
2022-09-26 15:07:19 -0300

    Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
@@ -23,6 +23,37 @@ let info = {
 } */
 
 //Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. 
-for (let key in info) {
+/* for (let key in info) {
   console.log(info[key]);
+} */
+
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. 
+
+/* let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
 }
+
+for (let key in info) {
+    console.log(info[key] + " e " + info2[key]);
+}
+ */
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+//Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+  console.log("O livro favorito de "+ leitor.nome+ ' '+ leitor.sobrenome+ ' se chama "'  + leitor.livrosFavoritos[0].titulo + '"');
