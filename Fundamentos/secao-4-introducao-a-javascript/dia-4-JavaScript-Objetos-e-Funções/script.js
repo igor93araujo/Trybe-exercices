3a5ca39fcd47ed06876ad8955cd95b9e3694f47a
Igor <igoraraujobda@gmail.com>
2022-09-26 11:46:05 -0300

    criação função de adição

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/script.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/script.js
@@ -1,4 +1,4 @@
-// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+/* // Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
 
 let names = {
   person1: 'João',
@@ -20,4 +20,61 @@ let car = {
 
 for (let key in car) {
   console.log(key + ': ' + car[key])
+} */
+
+//
+
+let statusDoCarro = 'desligado';
+let aceleracao = 0;
+let rotacaoVolante = 0;
+let setaDoCarro = 'desligada';
+
+function alterarStatus (){
+  if(statusDoCarro === 'desligado'){
+    statusDoCarro = 'ligado'
+  } else{
+    statusDoCarro = 'desligado';
+  }
+  return statusDoCarro;
 }
+
+function aceleracao(incremento){
+  aceleracao = aceleracao + incremento;
+  return 'Acelerando a ' + aceleracao + 'm/s²';
+}
+
+
+function frear(decremento){
+  aceleracao = aceleracao - decremento;
+  return 'Desacelerando a ' + aceleracao + 'm/s²';
+}
+
+
+function girarVolante(anguloRotacao){
+  if (anguloRotacao<0){
+    rotacaoVolante = 'esquerda';
+  } else if (anguloRotacao>0){
+    rotacaoVolante = 'direita';
+  } else {
+    rotação = 'nenhum lado';
+  }
+  return 'O volante virou para' + rotacaoVolante+ ' .';
+}
+
+function ligarSeta(sentido){
+  if(sentido === 'esquerda'){
+    return 'seta ligada para '+ sentido;
+  } else if (sentido === 'direita'){
+    return 'seta ligada para ' + sentido;
+  } else if (sentido === 'reto'){
+    return  'seta desligada.'
+  }
+}
+
+const a = 5;
+const b = 20;
+
+function soma(a,b){
+  return a+b;
+}
+console.log(soma(a,b))
