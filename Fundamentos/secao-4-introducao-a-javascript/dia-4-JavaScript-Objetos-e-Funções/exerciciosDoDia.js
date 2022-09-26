0b22f4ce5f8010c7c66643ed5405cdf8e70b138c
Igor <igoraraujobda@gmail.com>
2022-09-26 20:13:52 -0300

    contagem de numero que mais se repete

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
@@ -127,7 +127,7 @@ console.log(verySmaller([2, 4, 6, 7, 10, 0, -3])); */
 
 Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];. */
 
-function characCounter(arr) {
+/* function characCounter(arr) {
 
   let longest="";
 
@@ -139,4 +139,37 @@ function characCounter(arr) {
   return longest;
 }
 
-console.log(characCounter(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+console.log(characCounter(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); */
+
+
+
+
+/* Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+
+Valor esperado no retorno da função: 2. */
+
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+
+  for (let index in numeros) { // iterando por cada número no array de números
+    let verificaNumero = numeros[index]; // armazenando esse número em uma variável auxiliar
+    for (let index2 in numeros) { // iterando novamente por todos os números
+      if (verificaNumero === numeros[index2]) { // se encontrou o número do loop de fora
+        contNumero += 1; // incrementa o numero de vezes que esse número aparece
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+
+  return numeros[indexNumeroRepetido];
+}
+
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); // 2
