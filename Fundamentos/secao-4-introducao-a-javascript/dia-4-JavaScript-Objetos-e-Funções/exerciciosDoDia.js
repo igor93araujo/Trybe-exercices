ff0c04b19abadd06371af83611cf061fd9414d29
Igor <igoraraujobda@gmail.com>
2022-09-26 14:19:35 -0300

    Faça um for/in que mostre todas as chaves do objeto

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/exerciciosDoDia.js
@@ -1,11 +1,12 @@
 //Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome
-let info = {
+
+/* let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log("Bem-vinda, "+ info.personagem);
+console.log("Bem-vinda, "+ info.personagem); */
 
 //Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console.
 
@@ -14,5 +15,9 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'Sim'
-};
-console.log(info['recorrente']);
+}
+
+//Faça um for/in que mostre todas as chaves do objeto.
+for (let key in info) {
+    console.log(key);
+}
