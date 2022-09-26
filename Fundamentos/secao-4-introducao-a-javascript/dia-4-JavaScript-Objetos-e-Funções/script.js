1c06ef934a3fdacf159073eabec05e59f9a9ec72
Igor <igoraraujobda@gmail.com>
2022-09-26 09:39:20 -0300

    utilize for/in e imprima um console.log com as chaves e valores desse objeto

--- Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/script.js
+++ Fundamentos/secao-4-introducao-a-javascript/dia-4-JavaScript-Objetos-e-Funções/script.js
@@ -9,3 +9,15 @@ let names = {
 for (let key in names) {
    console.log('Olá' + ' ' + names[key])
 }
+
+// Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (let key in car) {
+  console.log(key, car[key])
+}
