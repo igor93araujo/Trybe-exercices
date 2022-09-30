435160e95ede2c3b3a3c083812244f590b87f9e8
Igor <igoraraujobda@gmail.com>
2022-09-30 19:39:03 -0300

    refiz a lista, capturei o pai, e refiz as classes

--- Fundamentos/secao-5-javaScript-DOM-e-seletores/dia-3-javascript-eventos/script.js
+++ Fundamentos/secao-5-javaScript-DOM-e-seletores/dia-3-javascript-eventos/script.js
@@ -10,21 +10,27 @@ function createDaysOfTheWeek() {
     weekDaysList.appendChild(dayListItem);
   };
 }
-
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
-function createDaysOfTheMonth() {
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-  for (let index = 1; index <= 2; index += 1) {
-    let monthDay = document.createElement('li');
-    days.appendChild(monthDay);
-    monthDay.innerHTML = index+28;
-  }
-  for (let index = 1; index <= 31; index += 1) {
-    let monthDay = document.createElement('li');
-    days.appendChild(monthDay);
-    monthDay.innerHTML = index;
+function createDaysOfTheMonth() {
+  let daysUl = document.getElementById('days');
+
+  for (let index = 0; index < decemberDaysList.length; index += 1) {
+    
+    let monthDay = decemberDaysList[index];
+    let dayItem = document.createElement('li');
+    dayItem.innerHTML = monthDay;
+    daysUl.appendChild(dayItem);
+
+    if(monthDay === 24 || monthDay === 25 || monthDay === 31){
+      dayItem.className = 'day holiday';
+    } else if (monthDay === 4 || monthDay === 11 || monthDay === 18 || monthDay === 25){
+      dayItem.className = 'day friday';
+    } else {
+      dayItem.className = 'day';
+    }
   }
 }
 createDaysOfTheMonth();
