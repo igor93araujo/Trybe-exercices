9257c808485352e4f95699b4b7b0d815c888eff9
Igor <igoraraujobda@gmail.com>
2022-10-03 19:39:53 -0300

    criação dos botões de aumentar e diminuir tamanho do texto, junto com suas funções

--- Fundamentos/secao-5-javaScript-DOM-e-seletores/dia-4-JavaScript-webStorage/script.js
+++ Fundamentos/secao-5-javaScript-DOM-e-seletores/dia-4-JavaScript-webStorage/script.js
@@ -1,7 +1,9 @@
 //variáveis fixas
 let getBody = document.getElementById('fullPage');
 let getBanner = document.querySelector('.banner');
+let text = document.querySelector('#Inner');
 
+//Código
 
 let ColorButton = document.createElement('button');
 getBanner.appendChild(ColorButton);
@@ -23,8 +25,56 @@ function changeBackgroundColor(){
   }
 }
 
+let textColorButton = document.createElement('button');
+getBanner.appendChild(textColorButton);
+textColorButton.innerHTML = "Trocar cor do texto";
+
+textColorButton.addEventListener("click",changeTextColor);
+
+function changeTextColor(){
+  let r = Math.floor(Math.random()*256);
+  let g = Math.floor(Math.random()*256);
+  let b = Math.floor(Math.random()*256);
+  let textColor =`rgb(${r},${g},${b})`;
+  text.style.color = textColor;
+}
+
+
+let increaseFontSizeButton = document.createElement('button');
+getBanner.appendChild(increaseFontSizeButton);
+increaseFontSizeButton.innerHTML = "Aumentar texto +";
+
+increaseFontSizeButton.addEventListener("click", increaseFontSize);
+
+let size = 15;
+let fullSize = '';
+function increaseFontSize(){
+size = size + 1;
+fullSize = `${size}px`
+console.log(fullSize);
+ text.style.fontSize = fullSize;
+}
+
+
+let decreaseFontSizeButton = document.createElement('button');
+getBanner.appendChild(decreaseFontSizeButton);
+decreaseFontSizeButton.innerHTML = "Diminuir texto -";
+
+decreaseFontSizeButton.addEventListener("click", decreaseFontSize);
+
+function decreaseFontSize(){
+size = size - 1;
+fullSize = `${size}px`
+console.log(fullSize);
+ text.style.fontSize = fullSize;
+}
+
+
+
 
 
 //chamadas de funções
 
 changeBackgroundColor();
+changeTextColor();
+changeFontSize();
