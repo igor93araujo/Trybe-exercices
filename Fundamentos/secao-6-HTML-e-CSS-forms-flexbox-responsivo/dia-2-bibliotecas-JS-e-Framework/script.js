ce14d0050bbd88951f161e2c7af2009929e2546f
Igor <igoraraujobda@gmail.com>
2022-10-11 18:08:38 -0300

    exercicios de fixação antes da aula

--- Fundamentos/secao-6-HTML-e-CSS-forms-flexbox-responsivo/dia-2-bibliotecas-JS-e-Framework/script.js
+++ Fundamentos/secao-6-HTML-e-CSS-forms-flexbox-responsivo/dia-2-bibliotecas-JS-e-Framework/script.js
@@ -1,35 +1,38 @@
-let check2 = document.getElementById('concordo2');
-
-const sendButton = document.getElementById('enviar'); //botao enviar
-/* sendButton.addEventListener('click', verifyAcess); */ //evento de click no botao
-
-function cancelSendButton(event){ //função de preventDefault
-  event.preventDefault();
-}
-
-
-/* function verifyAcess(){ //função para verificar acesso
-  if(check2.checked == false){
-    cancelSendButton() //preventDefault
-    alert('Seu formulário só será enviado se concordar com a opção 2')
-    console.log('clicou')
-  } 
-}
-verifyAcess( */
-sendButton.addEventListener('click', verifyData);
-
-/* function verifyData(){
-  const nameSize = document.getElementById('nome').value.length;
-  const emailSize = document.getElementById('email').value.length;
-  const textAreaSize = document.getElementById('textarea').value.length;
-  if(nameSize<10 || nameSize>40 || emailSize <10 || emailSize>50 || textAreaSize>500){
-    alert('Dados Inválidos')
-  } else{
-    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+   let check2 = document.getElementById('concordo2');
+  
+  let sendButton = document.getElementById('enviar'); //botao enviar
+  sendButton.disabled = true;
+  sendButton.style.opacity = '50%';
+  
+  check2.addEventListener('click', () => { //função para habilitar botao com click
+    if (sendButton.disabled === true) {
+      sendButton.disabled = false;
+      sendButton.style.opacity = '100%';
+    } else {
+      sendButton.disabled = true;
+      sendButton.style.opacity = '50%';
+    }
+  })
+  
+  
+  sendButton.addEventListener('click', (verifyData));
+  
+  function verifyData(){
+    const nameSize = document.getElementById('nome').value.length;
+    const emailSize = document.getElementById('email').value.length;
+    const textAreaSize = document.getElementById('textarea').value.length;
+    if(nameSize<10 || nameSize>40 || emailSize <10 || emailSize>50 || textAreaSize>500){
+      alert('Dados Inválidos')
+    } else{
+      alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+    }
   }
-}
-verifyData(); */
- 
+  verifyData(); 
+  
+
+
+
+
 
 
 
