4dc0d32c5ac364bc52bfadc1ae96f7fd8a0cc277
Igor <igoraraujobda@gmail.com>
2022-11-22 12:19:20 -0300

    ultimos exercicios

---
+++ Front End/secao-2-introducao-ao-react/dia-1-Hello-World-no-React/Fixação/exercicio-de-fixacao/src/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Igor Barbosa</h1>
+      <p> Dando os primeiros passos com react.</p>
+      <h2>Minhas Habilidades</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS Puro</li>
+        <li>Basic React</li>
+      </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
