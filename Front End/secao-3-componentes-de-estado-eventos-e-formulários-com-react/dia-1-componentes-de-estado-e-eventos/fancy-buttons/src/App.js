cac3f10209f78684ba52e368a2bea8f70774df09
Igor <igoraraujobda@gmail.com>
2022-11-29 19:09:44 -0300

    exercicio de fixação

--- Front End/secao-3-componentes-de-estado-eventos-e-formulários-com-react/dia-1-componentes-de-estado-e-eventos/fancy-buttons/src/App.js
+++ Front End/secao-3-componentes-de-estado-eventos-e-formulários-com-react/dia-1-componentes-de-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,9 +1,60 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>oi</h1>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.exibeConsole1 = this.exibeConsole1.bind(this);
+    this.exibeConsole2 = this.exibeConsole2.bind(this);
+    this.exibeConsole3 = this.exibeConsole3.bind(this);
+
+    this.state = {
+      clicaBtn1: 0,
+      clicaBtn2: 0,
+      clicaBtn3: 0,
+    };
+  }
+
+  getButtonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
+  exibeConsole1 = () => {
+    const { clicaBtn1 } = this.state;
+    this.setState((prevState) => ({
+      clicaBtn1: prevState.clicaBtn1 + 1,
+    }), () => {
+      console.log(`Botao 1 ${this.getButtonColor(clicaBtn1)}`);
+    });
+  };
+
+  exibeConsole2 = () => {
+    const { clicaBtn2 } = this.state;
+    this.setState((prevState) => ({
+      clicaBtn2: prevState.clicaBtn2 + 1,
+    }), () => {
+      console.log(`Botao 2 ${this.getButtonColor(clicaBtn2)}`);
+    })
+  };
+  exibeConsole3 = () => {
+    const { clicaBtn3 } = this.state;
+    this.setState((prevState) => ({
+      clicaBtn3: prevState.clicaBtn3 + 1,
+    }), () => {
+      console.log(`Botao 3 ${this.getButtonColor(clicaBtn3)}`);
+    })
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.exibeConsole1}>botao1</button>
+        <button onClick={this.exibeConsole2}>botao2</button>
+        <button onClick={this.exibeConsole3}>botao3</button>
+      </div>
+    );
+  }
 }
 
 export default App;
