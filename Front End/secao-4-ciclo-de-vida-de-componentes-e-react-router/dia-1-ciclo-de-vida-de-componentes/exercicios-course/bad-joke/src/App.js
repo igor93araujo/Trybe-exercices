aa5d0b79bc7d4b9860a89cee83d0f9505bba0dee
Igor <igoraraujobda@gmail.com>
2022-12-09 08:35:37 -0300

    deletando um exercicio nao realizado

---
+++ Front End/secao-4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercicios-course/bad-joke/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DeadJoke from './components/DeadJoke.jsx';
+import Joke from './components/Jokes';
+
+
+function App() {
+  return (
+    <div className="App">
+      <DeadJoke />
+      <Joke />
+    </div>
+  );
+}
+
+export default App;
